6eb197733aa10c4b556a4fa0c325134e859e4dc7
Neelasha <neelashabaa@gmail.com>
2024-12-15 02:01:27 +0530

    deployment 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
-  base: 'https://neelstoned.github.io/React_Weather_App/'
+  plugins: [react()],
+  base:  "https://github.com/neelstoned/React_Weather_App"
 })
